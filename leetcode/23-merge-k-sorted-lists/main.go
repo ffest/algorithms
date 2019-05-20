dda63ff17fa22c20631ab249d3eee76081c990e4
Vladimir <bryksinvladimir@gmail.com>
2019-05-20 18:38:37 +0700

    23 leetcode's refactored O(nlogn) solution.

--- leetcode/23-merge-k-sorted-lists/main.go
+++ leetcode/23-merge-k-sorted-lists/main.go
@@ -16,30 +16,52 @@ func mergeKLists(lists []*ListNode) *ListNode {
 	if len(lists) == 0 {
 		return nil
 	}
-	result := new(ListNode)
+	elements := make([]int, 0)
+	for i := 0; i < len(lists); i++ {
+		for lists[i] != nil {
+			elements = append(elements, lists[i].Val)
+			lists[i] = lists[i].Next
+		}
+	}
+	if len(elements) == 0 {
+		return nil
+	}
+	elements = Quick(elements)
+
+	result := &ListNode{Val: elements[0]}
 	subres := result
-	for {
-		min := 1<<31 - 1
-		minIdx := 0
-		for i := 0; i < len(lists); i++ {
-			if lists[i] == nil {
-				continue
-			}
-			if min > lists[i].Val {
-				min = lists[i].Val
-				minIdx = i
-			}
-		}
-		next := lists[minIdx]
-		if next == nil {
-			break
-		}
-		subres.Next = next
+
+	for i := 1; i < len(elements); i++ {
+		subres.Next = &ListNode{Val: elements[i]}
 		subres = subres.Next
-		lists[minIdx] = lists[minIdx].Next
 	}
 
-	return result.Next
+	return result
+}
+
+func Quick(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	left, right := 0, len(arr)-1
+	median := (1<<31 - 1) % len(arr)
+
+	arr[median], arr[right] = arr[right], arr[median]
+
+	for i := range arr {
+		if arr[i] < arr[right] {
+			arr[left], arr[i] = arr[i], arr[left]
+			left++
+		}
+	}
+
+	arr[left], arr[right] = arr[right], arr[left]
+
+	Quick(arr[:left])
+	Quick(arr[left+1:])
+
+	return arr
 }
 
 func main() {
