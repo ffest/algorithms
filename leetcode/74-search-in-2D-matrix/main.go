1414b42a4c42c9068e97afc2dba3facaef0d196e
Vladimir <bryksinvladimir@gmail.com>
2019-05-27 14:53:14 +0700

    74 leetcode's refactored

--- leetcode/74-search-in-2D-matrix/main.go
+++ leetcode/74-search-in-2D-matrix/main.go
@@ -2,29 +2,49 @@ package main
 
 import "log"
 
-// TODO: To be refactored
 func searchMatrix(matrix [][]int, target int) bool {
 	if len(matrix) == 0 || len(matrix[0]) == 0 {
 		return false
 	}
+
+	n := len(matrix)
 	m := len(matrix[0])
-	var i int
-	for i < len(matrix)-1 && matrix[i][m-1] < target {
-		i++
+	if target < matrix[0][0] || target > matrix[n-1][m-1] {
+		return false
 	}
-	for j := 0; j < m; j++ {
-		if matrix[i][j] == target {
+
+	var mid int
+	low, high := 0, n-1
+	for low <= high {
+		mid = (low + high) / 2
+		if matrix[mid][0] > target {
+			high = mid - 1
+		} else if matrix[mid][0] < target {
+			low = mid + 1
+		} else {
 			return true
 		}
 	}
+
+	left, right := 0, m-1
+	for left <= right {
+		mid = (left + right) / 2
+		if matrix[high][mid] > target {
+			right = mid - 1
+		} else if matrix[high][mid] < target {
+			left = mid + 1
+		} else {
+			return true
+		}
+	}
+
 	return false
 }
 
 func main() {
 	matrix := [][]int{
-		{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15},
+		{1, 2, 3, 4, 5}, {6, 7, 8, 8, 10}, {11, 12, 13, 14, 15},
 	}
-	matrix = [][]int{}
-	target := 10
+	target := 4
 	log.Print(searchMatrix(matrix, target))
 }
