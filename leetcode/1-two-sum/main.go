6fc3c4758f2b32e072becd5c769b57bc95d0a8f9
Vladimir <bryksinvladimir@gmail.com>
2019-05-30 13:23:10 +0700

    1 leetcode's refactored

--- leetcode/1-two-sum/main.go
+++ leetcode/1-two-sum/main.go
@@ -2,16 +2,15 @@ package main
 
 import "fmt"
 
-// TODO: make it O(n)
 func twoSum(nums []int, target int) []int {
-	for i := 0; i < len(nums)-1; i++ {
-		for j := i + 1; j < len(nums); j++ {
-			if nums[i]+nums[j] == target {
-				return []int{i, j}
-			}
+	cache := make(map[int]int)
+	for i := range nums {
+		if idx, ok := cache[target-nums[i]]; ok {
+			return []int{idx, i}
 		}
+		cache[nums[i]] = i
 	}
-	return []int{0, 0}
+	return []int{}
 }
 
 func main() {
