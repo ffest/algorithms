9a809135ce57a19918454a9f9840b232b18d60a2
Vladimir <bryksinvladimir@gmail.com>
2019-06-05 15:37:29 +0300

    5 leetcode's added

--- leetcode/5-longest-palindromic-substring/main.go
+++ leetcode/5-longest-palindromic-substring/main.go
@@ -2,56 +2,55 @@ package main
 
 import (
 	"fmt"
-	"log"
 )
 
-func findMaxPalindrom(str string) string {
-	if len(str) <= 2 {
-		return str[:1]
+func longestPalindrome(str string) string {
+	if len(str) == 0 {
+		return ""
+	} else if len(str) == 1 {
+		return str
 	}
 
-	strBytes := []byte(str)
+	dp := make([][]bool, len(str))
+	for i := range dp {
+		dp[i] = make([]bool, len(str))
+	}
 
-	var max []byte
-	for i := range strBytes {
-		if i == 0 || i == len(strBytes)-1 {
-			continue
-		}
+	var maxPalindpom string
+	var maxLength int
 
-		var left int
-		var right int
-		var localMax int
-		left = i
-		if strBytes[i] == strBytes[i+1] {
-			right = i + 1
-			localMax = 2
-		} else {
-			right = i
-			localMax = 1
-		}
+	// case for length of 1
+	for i := 0; i < len(str); i++ {
+		dp[i][i] = true
+	}
+	maxPalindpom = str[:1]
+	maxLength = 1
 
-		for {
-			if left == 0 || right >= len(strBytes)-1 {
-				break
-			}
-
-			left--
-			right++
-			if strBytes[left] != strBytes[right] {
-				break
-			}
-			localMax += 2
-		}
-
-		if localMax > len(max) {
-			log.Print(i)
-			log.Print(string(strBytes[left : right+1]))
-			max = strBytes[left : right+1]
+	// case for length of 2
+	for i := 1; i < len(str); i++ {
+		if str[i] == str[i-1] {
+			maxPalindpom = str[i-1 : i+1]
+			maxLength = 2
+			dp[i-1][i] = true
 		}
 	}
-	return string(max)
+
+	// case for length of greater than 2
+	for i := 2; i < len(str); i++ {
+		for j := 0; j <= i-2; j++ {
+			if str[i] == str[j] && dp[j+1][i-1] {
+				dp[j][i] = true
+				if i-j+1 > maxLength {
+					maxLength = i - j + 1
+					maxPalindpom = str[j : i+1]
+				}
+			}
+		}
+	}
+
+	return maxPalindpom
 }
 
 func main() {
-	fmt.Println(findMaxPalindrom("ccaba"))
+	fmt.Println(longestPalindrome("cababac"))
 }
