21e25499bab20c2502355dbe822a302fe6949ea7
Vladimir <bryksinvladimir@gmail.com>
2019-06-21 10:22:05 +0300

    17 leetcode's added

---
+++ leetcode/17-letter-combination-of-a-phone-number/main.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func letterCombinations(digits string) []string {
+	if len(digits) == 0 {
+		return []string{}
+	}
+	mapping := map[byte][]byte{
+		'2': {'a', 'b', 'c'},
+		'3': {'d', 'e', 'f'},
+		'4': {'g', 'h', 'i'},
+		'5': {'j', 'k', 'l'},
+		'6': {'m', 'n', 'o'},
+		'7': {'p', 'q', 'r', 's'},
+		'8': {'t', 'u', 'v'},
+		'9': {'w', 'x', 'y', 'z'},
+	}
+	result := make([]string, 0)
+	combinations("", digits, 0, mapping, &result)
+	return result
+}
+
+func combinations(current, digits string, level int, mapping map[byte][]byte, result *[]string) {
+	if len(current) == len(digits) {
+		*result = append(*result, current)
+		return
+	}
+
+	letters := mapping[digits[level]]
+	for _, l := range letters {
+		combinations(current+string(l), digits, level+1, mapping, result)
+	}
+}
+
+func main() {
+	input := "23"
+	fmt.Println(letterCombinations(input))
+}
