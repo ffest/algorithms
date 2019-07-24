d93b7b7e6bf685f7275ad96a018aa9c00486b3e2
Vladimir <bryksinvladimir@gmail.com>
2019-07-24 09:27:25 +0300

    105 leetcode's added

---
+++ leetcode/105-construct-binary-tree-from-preorder-and-inorder-traversal/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+/**
+ * Definition for a binary tree node.
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func buildTree(preorder []int, inorder []int) *TreeNode {
+	if len(preorder) == 0 || len(inorder) == 0 {
+		return nil
+	}
+	rootVal := preorder[0]
+	root := &TreeNode{
+		Val: rootVal,
+	}
+
+	var i int
+	for i = 0; i < len(inorder); i++ {
+		if inorder[i] == rootVal {
+			break
+		}
+	}
+	root.Left = buildTree(preorder[1:i+1], inorder[:i])
+	root.Right = buildTree(preorder[i+1:], inorder[i+1:])
+	return root
+}
+
+func main() {
+	preorder := []int{3, 9, 20, 15, 7}
+	inorder := []int{9, 3, 15, 20, 7}
+	fmt.Print(buildTree(preorder, inorder))
+}
