15da6ab6f95383963da29139fbd716d912a63b3d
Vladimir <bryksinvladimir@gmail.com>
2019-07-26 17:41:38 +0300

    235 leetcode's added

---
+++ leetcode/235-lowest-common-ancestor-of-a-binary-search-tree/main.go
@@ -0,0 +1,53 @@
+package main
+
+import "fmt"
+
+/**
+ * Definition for TreeNode.
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	for {
+		if root.Val < p.Val && root.Val < q.Val {
+			root = root.Right
+		} else if root.Val > p.Val && root.Val > q.Val {
+			root = root.Left
+		} else {
+			return root
+		}
+	}
+}
+
+func main() {
+	p := &TreeNode{
+		Val: 5,
+		Left: &TreeNode{
+			Val: 6,
+		},
+		Right: &TreeNode{
+			Val: 2,
+			Left: &TreeNode{
+				Val: 7,
+			},
+			Right: &TreeNode{
+				Val: 4,
+			},
+		},
+	}
+	q := &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val: 0,
+		},
+		Right: &TreeNode{
+			Val: 8,
+		},
+	}
+	root := &TreeNode{Val: 3, Left: p, Right: q}
+	fmt.Print(lowestCommonAncestor(root, p, q))
+}
