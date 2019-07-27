c56104e78cc253ada584f8a8c5ee7b11bc205e49
Vladimir <bryksinvladimir@gmail.com>
2019-07-27 18:38:29 +0300

    189 leetcode's added

---
+++ leetcode/189-rotate-array/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+func rotate(nums []int, k int) {
+	if len(nums) == 0 || k == 0 {
+		return
+	}
+	k = k % len(nums)
+	reverse(nums, 0, len(nums)-1)
+	reverse(nums, 0, k-1)
+	reverse(nums, k, len(nums)-1)
+}
+
+func reverse(nums []int, start, end int) {
+	for start < end {
+		nums[start], nums[end] = nums[end], nums[start]
+		start++
+		end--
+	}
+}
+
+func main() {
+	nums := []int{1, 2, 3, 4, 5, 6, 7}
+	k := 3
+	rotate(nums, k)
+	fmt.Println(nums)
+}
