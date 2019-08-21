713557f3ff077950908e41a7d6d48a76b67a8d15
Vladimir <bryksinvladimir@gmail.com>
2019-08-21 22:37:57 +0700

    154 leetcode's added

---
+++ leetcode/154-find-minimum-in-rotated-sorted-array-II/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func findMin(nums []int) int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		mid := (left+right)/2
+		if nums[mid] > nums[right] {
+			left = mid + 1
+		} else if nums[mid] > nums[right] {
+			right = mid
+		} else {
+			right--
+		}
+	}
+
+	return nums[left]
+}
+
+func main() {
+	nums := []int{4, 5, 6, 7, 8, 1, 1, 2, 2}
+	fmt.Println(findMin(nums))
+}
