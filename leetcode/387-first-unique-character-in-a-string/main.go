d611309786f8644c8688b0db04c7a0f40e847e8e
Vladimir <bryksinvladimir@gmail.com>
2019-09-13 15:08:43 +0700

    387 leetcode's refactored

--- leetcode/387-first-unique-character-in-a-string/main.go
+++ leetcode/387-first-unique-character-in-a-string/main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
-func firstUniqChar(s string) int {
+// solution based on map
+/*func firstUniqChar(s string) int {
 	cache := make(map[int32]int)
 	for i, l := range s {
 		if _, ok := cache[l]; ok {
@@ -15,16 +15,32 @@ func firstUniqChar(s string) int {
 		cache[l] = i
 	}
 
-	firstLetter := math.MaxInt32
+	first := math.MaxInt32
 	for _, v := range cache {
-		if v < firstLetter && v >= 0 {
-			firstLetter = v
+		if v != -1 && first > v {
+			first = v
 		}
 	}
-	if firstLetter == math.MaxInt32 {
+	if first == math.MaxInt32 {
 		return -1
 	}
-	return firstLetter
+	return first
+}*/
+
+// solution based on [26]int{}
+func firstUniqChar(s string) int {
+	cache := [26]int{}
+	for _, l := range s {
+		cache[l-'a']++
+	}
+
+	for i := range s {
+		if cache[s[i]-'a'] == 1 {
+			return i
+		}
+	}
+
+	return -1
 }
 
 func main() {
