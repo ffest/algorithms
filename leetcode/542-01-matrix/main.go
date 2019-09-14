bda11f8560f0fb7b36147841c4d0630b8693412b
Vladimir <bryksinvladimir@gmail.com>
2019-09-14 18:34:02 +0700

    542 leetcode's refactored

--- leetcode/542-01-matrix/main.go
+++ leetcode/542-01-matrix/main.go
@@ -8,6 +8,7 @@ func updateMatrix(matrix [][]int) [][]int {
 	if len(matrix) == 0 || len(matrix[0]) == 0 {
 		return matrix
 	}
+
 	n, m := len(matrix), len(matrix[0])
 	coordinates := make([][2]int, 0)
 	for i := 0; i < n; i++ {
@@ -19,21 +20,23 @@ func updateMatrix(matrix [][]int) [][]int {
 			matrix[i][j] = 1<<31 - 1
 		}
 	}
-	ways := [][]int{{-1, 0}, {1, 0}, {0, -1}, {0, 1}}
+
+	ways := [][2]int{{1, 0}, {-1, 0}, {0, 1}, {0, -1}}
 	for len(coordinates) > 0 {
 		current := coordinates[0]
-		currentVal := matrix[current[0]][current[1]]
 		coordinates = coordinates[1:]
+		currentVal := matrix[current[0]][current[1]]
+
 		for _, way := range ways {
-			i := current[0] + way[0]
-			j := current[1] + way[1]
-			if i < 0 || i == n || j < 0 || j == m || matrix[i][j] == 0 || matrix[i][j] <= currentVal+1 {
+			i, j := current[0]+way[0], current[1]+way[1]
+			if i < 0 || j < 0 || i == n || j == m || matrix[i][j] == 0 || matrix[i][j] < currentVal+1 {
 				continue
 			}
 			matrix[i][j] = currentVal + 1
 			coordinates = append(coordinates, [2]int{i, j})
 		}
 	}
+
 	return matrix
 }
 
