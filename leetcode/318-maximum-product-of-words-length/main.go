dfceccb016a7f89d1ed0ff716202d2c12f889a60
Vladimir <bryksinvladimir@gmail.com>
2019-09-18 12:18:05 +0700

    318 leetcode's refactored

--- leetcode/318-maximum-product-of-words-length/main.go
+++ leetcode/318-maximum-product-of-words-length/main.go
@@ -6,14 +6,15 @@ import (
 
 func maxProduct(words []string) int {
 	var max int
-	masks := make([]int64, len(words))
-	for i := range words {
-		for j := range words[i] {
-			masks[i] |= 1 << (words[i][j] - 'a')
+	masks := make([]int, len(words))
+	for i, word := range words {
+		for j := range word {
+			masks[i] |= 1 << (word[j] - 'a')
 		}
 	}
-	for i := range masks {
-		for j := range masks {
+
+	for i := 0; i < len(masks); i++ {
+		for j := 0; j < len(masks); j++ {
 			if masks[i]&masks[j] == 0 && max < len(words[i])*len(words[j]) {
 				max = len(words[i]) * len(words[j])
 			}
