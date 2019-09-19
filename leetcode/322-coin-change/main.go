552fd8da3d64b6667a3ed67d8cdc8acd8b766937
Vladimir <bryksinvladimir@gmail.com>
2019-09-19 10:54:25 +0700

    322 leetcode's refactored

--- leetcode/322-coin-change/main.go
+++ leetcode/322-coin-change/main.go
@@ -6,11 +6,12 @@ func coinChange(coins []int, amount int) int {
 	dp := make([]int, amount+1)
 	for i := 1; i <= amount; i++ {
 		min := -1
-		for _, c := range coins {
-			if i >= c && dp[i-c] != -1 {
-				if min == -1 || dp[i-c]+1 < min {
-					min = dp[i-c] + 1
-				}
+		for _, coin := range coins {
+			if i < coin || dp[i-coin] == -1 {
+				continue
+			}
+			if min == -1 || dp[i-coin]+1 < min {
+				min = dp[i-coin] + 1
 			}
 		}
 		dp[i] = min
