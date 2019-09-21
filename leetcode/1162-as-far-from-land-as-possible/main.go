52ec7be6369425b9d35076b0fd8ca5126f6667b7
Vladimir <bryksinvladimir@gmail.com>
2019-09-21 13:45:54 +0700

    1162 leetcode's refactored

--- leetcode/1162-as-far-from-land-as-possible/main.go
+++ leetcode/1162-as-far-from-land-as-possible/main.go
@@ -2,49 +2,43 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func maxDistance(grid [][]int) int {
-	earthes := make([][2]int, 0)
+	queue := make([][2]int, 0)
+
 	for i := 0; i < len(grid); i++ {
 		for j := 0; j < len(grid[0]); j++ {
 			if grid[i][j] == 1 {
-				earthes = append(earthes, [2]int{i, j})
+				queue = append(queue, [2]int{i + 1, j}, [2]int{i - 1, j}, [2]int{i, j + 1}, [2]int{i, j - 1})
 			}
 		}
 	}
-	if len(earthes) == 0 || len(earthes) == len(grid)*len(grid[0]) {
+	if len(queue) == 0 || len(grid)*len(grid[0]) == len(queue)/4 {
 		return -1
 	}
-	var maxDistance int
-	for i := 0; i < len(grid); i++ {
-		for j := 0; j < len(grid[0]); j++ {
-			if grid[i][j] == 1 {
-				continue
-			}
-
-			currentMin := math.MaxInt32
-			for _, earth := range earthes {
-				current := int(math.Abs(float64(earth[0]-i)) + math.Abs(float64(earth[1]-j)))
-				if current < currentMin {
-					currentMin = current
-				}
-			}
-			if currentMin > maxDistance {
-				maxDistance = currentMin
+	var steps int
+	for len(queue) > 0 {
+		steps++
+		qSize := len(queue)
+		for k := 0; k < qSize; k++ {
+			current := queue[0]
+			queue = queue[1:]
+			i, j := current[0], current[1]
+			if i >= 0 && j >= 0 && i < len(grid) && j < len(grid[0]) && grid[i][j] == 0 {
+				grid[i][j] = steps
+				queue = append(queue, [2]int{i + 1, j}, [2]int{i - 1, j}, [2]int{i, j + 1}, [2]int{i, j - 1})
 			}
 		}
 	}
-
-	return maxDistance
+	return steps - 1
 }
 
 func main() {
 	grid := [][]int{
 		{0, 0, 0},
 		{0, 0, 0},
-		{0, 0, 0},
+		{1, 0, 0},
 	}
 	fmt.Println(maxDistance(grid))
 }
