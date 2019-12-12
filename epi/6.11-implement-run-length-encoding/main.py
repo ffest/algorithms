7aa7ef9fd85aeaf082c7df4955f857e5090e4497
Vladimir <bryksinvladimir@gmail.com>
2019-12-12 10:39:04 +0700

    6.11 epi's added

---
+++ epi/6.11-implement-run-length-encoding/main.py
@@ -0,0 +1,24 @@
+def decoding(s: str) -> str:
+    count, result = 0, []
+    for c in s:
+        if c.isdigit():
+            count = count*10+int(c)
+        else:
+            result.append(c * count)
+            count = 0
+    return "".join(result)
+
+
+def encoding(s: str) -> str:
+    count, result = 1, []
+    for i in range(1, len(s)+1):
+        if i == len(s) or s[i] != s[i-1]:
+            result.append(str(count) + s[i-1])
+            count = 1
+        else:
+            count += 1
+    return "".join(result)
+
+
+print(decoding("4a2b1e4d4a"))
+print(encoding("aaaewwrraaadd"))
