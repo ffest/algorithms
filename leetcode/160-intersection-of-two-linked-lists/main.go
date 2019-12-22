aecec944c23fa4eb5783bb33d4ad83cc824ee458
Vladimir <bryksinvladimir@gmail.com>
2019-12-22 21:59:14 +0700

    160 leetcode's added

--- leetcode/160-intersection-of-two-linked-lists/main.go
+++ leetcode/160-intersection-of-two-linked-lists/main.go
@@ -10,7 +10,8 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func getIntersectionNode(headA, headB *ListNode) *ListNode {
+// Without calculating lengths of lists
+/*func getIntersectionNode(headA, headB *ListNode) *ListNode {
 	if headA == nil || headB == nil {
 		return nil
 	}
@@ -37,6 +38,37 @@ func getIntersectionNode(headA, headB *ListNode) *ListNode {
 		q = q.Next
 	}
 	return nil
+}*/
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+
+	lenA, lenB := lengthOfList(headA), lengthOfList(headB)
+	if lenA < lenB {
+		headA, headB = headB, headA
+		lenA, lenB = lenB, lenA
+	}
+	for i := 0; i < lenA-lenB; i++ {
+		headA = headA.Next
+	}
+
+	for headA != nil && headB != nil && headA != headB {
+		headA = headA.Next
+		headB = headB.Next
+	}
+
+	return headA
+}
+
+func lengthOfList(l *ListNode) int {
+	var length int
+	for l != nil {
+		l = l.Next
+		length++
+	}
+	return length
 }
 
 func main() {
