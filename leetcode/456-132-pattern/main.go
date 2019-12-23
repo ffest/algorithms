f3cdda5ba822af58583c1cf5300ac460111024eb
Vladimir <bryksinvladimir@gmail.com>
2019-12-23 17:03:25 +0700

    456 leetcode's added

--- leetcode/456-132-pattern/main.go
+++ leetcode/456-132-pattern/main.go
@@ -25,8 +25,17 @@ import (
 
 // Solution using stack
 func find132pattern(nums []int) bool {
-	if len(nums) < 3 {
-		return false
+	max := -1 << 31
+	stack := make([]int, 0)
+	for i := len(nums) - 1; i >= 0; i-- {
+		if nums[i] < max {
+			return true
+		}
+		for len(stack) > 0 && stack[len(stack)-1] < nums[i] {
+			max = stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+		}
+		stack = append(stack, nums[i])
 	}
 	return false
 }
