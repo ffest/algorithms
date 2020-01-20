57671706ae7d7f2b8c3256783bb7296fd4002b54
Vladimir <bryksinvladimir@gmail.com>
2020-01-20 13:51:28 +0700

    264 leetcode's added

---
+++ leetcode/264-ugly-number-II/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func nthUglyNumber(n int) int {
+	if n == 1 {
+		return n
+	}
+	fives, threes, twos := 0, 0, 0
+	dp := make([]int, n)
+	dp[0] = 1
+	for i := 1; i < n; i++ {
+		dp[i] = min(dp[twos]*2, min(dp[threes]*3, dp[fives]*5))
+		if dp[i] == dp[twos]*2 {
+			twos++
+		}
+		if dp[i] == dp[threes]*3 {
+			threes++
+		}
+		if dp[i] == dp[fives]*5 {
+			fives++
+		}
+	}
+	return dp[n-1]
+}
+
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func main() {
+	fmt.Println(nthUglyNumber(10))
+}
