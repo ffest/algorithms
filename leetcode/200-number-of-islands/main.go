20507d7de9147ee4c67889ca0f20004b4db37a4d
Vladimir <bryksinvladimir@gmail.com>
2020-03-05 15:37:45 +0700

    200 leetcode's refactored

--- leetcode/200-number-of-islands/main.go
+++ leetcode/200-number-of-islands/main.go
@@ -11,26 +11,24 @@ func numIslands(grid [][]byte) int {
 	var count int
 	for i := 0; i < len(grid); i++ {
 		for j := 0; j < len(grid[0]); j++ {
-			if grid[i][j] != '1' {
-				continue
+			if grid[i][j] == '1' {
+				dfs(i, j, grid)
+				count++
 			}
-
-			count++
-			helper(grid, i, j)
 		}
 	}
 	return count
 }
 
-func helper(grid [][]byte, i, j int) {
+func dfs(i, j int, grid [][]byte) {
 	if i < 0 || j < 0 || i == len(grid) || j == len(grid[0]) || grid[i][j] != '1' {
 		return
 	}
 	grid[i][j]++
-	helper(grid, i+1, j)
-	helper(grid, i-1, j)
-	helper(grid, i, j+1)
-	helper(grid, i, j-1)
+	dfs(i+1, j, grid)
+	dfs(i-1, j, grid)
+	dfs(i, j+1, grid)
+	dfs(i, j-1, grid)
 }
 
 func main() {
