955ff3c9eea80ff1fac9e8db77ab64a578a9af57
Vladimir <bryksinvladimir@gmail.com>
2020-03-17 19:42:01 +0700

    155 leetcode's refactored

--- leetcode/155-min-stack/main.go
+++ leetcode/155-min-stack/main.go
@@ -3,28 +3,22 @@ package main
 import "log"
 
 type MinStack struct {
-	helper   []int
-	stack    []int
-	elements map[int]int
+	helper []int
+	stack  []int
 }
 
 /** initialize your data structure here. */
 func Constructor() MinStack {
 	return MinStack{
-		stack:    make([]int, 0),
-		helper:   make([]int, 0),
-		elements: make(map[int]int),
+		stack:  make([]int, 0),
+		helper: make([]int, 0),
 	}
 }
 
 func (ms *MinStack) Push(x int) {
 	ms.stack = append(ms.stack, x)
-
-	if len(ms.helper) == 0 || ms.helper[len(ms.helper)-1] > x {
+	if len(ms.helper) == 0 || ms.GetMin() >= x {
 		ms.helper = append(ms.helper, x)
-	}
-	if ms.helper[len(ms.helper)-1] == x {
-		ms.elements[x]++
 	}
 }
 
@@ -32,38 +26,31 @@ func (ms *MinStack) Pop() {
 	if len(ms.stack) == 0 {
 		return
 	}
-	toPop := ms.stack[len(ms.stack)-1]
-	ms.stack = ms.stack[:len(ms.stack)-1]
-
-	if toPop == ms.helper[len(ms.helper)-1] {
-		if ms.elements[toPop] == 1 {
-			ms.helper = ms.helper[:len(ms.helper)-1]
-		}
-		ms.elements[toPop]--
+	if ms.stack[len(ms.stack)-1] == ms.GetMin() {
+		ms.helper = ms.helper[:len(ms.helper)-1]
 	}
+	ms.stack = ms.stack[:len(ms.stack)-1]
 }
 
 func (ms *MinStack) Top() int {
-	if len(ms.stack) == 0 {
-		return 0
-	}
 	return ms.stack[len(ms.stack)-1]
 }
 
 func (ms *MinStack) GetMin() int {
-	if len(ms.helper) == 0 {
-		return 0
-	}
 	return ms.helper[len(ms.helper)-1]
 }
 
 func main() {
 	minStack := Constructor()
-	minStack.Push(-2)
+	minStack.Push(2)
 	minStack.Push(0)
-	minStack.Push(-3)
+	minStack.Push(3)
+	minStack.Push(0)
 	log.Print(minStack.GetMin())
 	minStack.Pop()
-	log.Print(minStack.Top())
+	log.Print(minStack.GetMin())
+	minStack.Pop()
+	log.Print(minStack.GetMin())
+	minStack.Pop()
 	log.Print(minStack.GetMin())
 }
