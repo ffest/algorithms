ed138d675cc73676860a08db7c153c4d8a9e5a41
Vladimir Bryksin <bryksiv@amazon.com>
2021-10-22 14:20:55 -0700

    37 leetcode's added

---
+++ leetcode/37-sudoku-solver/main.go
@@ -0,0 +1,54 @@
+package main
+
+func solveSudoku(board [][]byte) {
+	solve(board)
+}
+
+func solve(board [][]byte) bool {
+	for i := 0; i < 9; i++ {
+		for j := 0; j < 9; j++ {
+			if board[i][j] != '.' {
+				continue
+			}
+			for c := byte('1'); c <= byte('9'); c++ {
+				if isValid(board, i, j, c) {
+					board[i][j] = c
+					if solve(board) {
+						return true
+					} else {
+						board[i][j] = '.'
+					}
+				}
+			}
+			return false
+		}
+	}
+	return true
+}
+
+func isValid(board [][]byte, row, col int, c byte) bool {
+	for i := 0; i < 9; i++ {
+		if board[i][col] == c || board[row][i] == c {
+			return false
+		}
+		if board[row/3*3+i/3][col/3*3+i%3] == c {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	board := [][]byte{
+		{'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+		{'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+		{'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+		{'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+		{'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+		{'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+		{'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+		{'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+		{'.', '.', '.', '.', '8', '.', '.', '7', '9'},
+	}
+	solveSudoku(board)
+}
